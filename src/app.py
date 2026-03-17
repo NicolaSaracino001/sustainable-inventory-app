470044dc7e39ed7f3718a2d7a54a1fa1e6f5a153
Nicola Saracino <nicola.saracino098@gmail.com>
2026-03-17 19:49:31 +0100

    Fase 9.1: Fix ModuleNotFoundError e initsrc.

--- src/app.py
+++ src/app.py
@@ -2,42 +2,39 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
+import sys
 from dotenv import load_dotenv
 
-# Carica le configurazioni dal file .env
+# Aggiunge la cartella principale al percorso di ricerca di Python
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 load_dotenv()
 
-# Inizializziamo l'oggetto Database
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # CONFIGURAZIONE
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # COLLEGIAMO IL DATABASE ALL'APP
     db.init_app(app)
 
-    # CONFIGURAZIONE LOGIN
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    # QUESTA È LA PARTE MANCANTE: Il caricatore utente
+    # Importiamo i modelli usando il percorso corretto
     from src.models.models import User
     
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # REGISTRAZIONE DEI BLUEPRINTS
     from src.routes.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # Rotta di prova
     @app.route('/')
     def index():
         return "<h1>S.I.M. Acceso!</h1><a href='/login'>Vai al Login</a>"
